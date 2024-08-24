468c08b2652be2cec3c8c791c8e69c4d3b53b9e1
Gean Filho <98719228+GeanFilho@users.noreply.github.com>
2024-08-24 17:15:06 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Retangulo Retangulo1 = new Retangulo(3.0, 5.9);
+        Retangulo Retangulo2 = new Retangulo(6.0, 4.5);
+
+        System.out.println("Retangulo 1:");
+        Retangulo1.Informacao();
+
+        System.out.println();
+
+
+        System.out.println("Retangulo 2:");
+        Retangulo2.Informacao();
+
+
+    }
+}
